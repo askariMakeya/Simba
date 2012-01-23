92e146eca7b51f20f1c9b7a223e41d3c0c411c1a
Rebecca Odim <bexin7@gmail.com>
2012-01-23 02:43:49 -0500

    timeline

---
+++ timeline/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Timeline Example</title>
+		<link href="timeline.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div id="login">Login Options</div>
+				<div id="search">Search for a topic</div>
+				<div id="nav">
+					<ul>
+						<li>2012 Election</li>
+						<li>Civil Rights</li>
+						<li>Defense</li>
+						<li>Disabilities</li>
+						<li>Education</li>
+						<li>Health Care</li>
+						<li>Economy</li>
+						<li>Energy</li>
+						<li>Foreign Policy</li>
+						<li>Immigration</li>
+						<li>Poverty</li>
+					</ul>
+				</div>
+			</div>
+			<div id="content">
+				<h1>Climate Change</h1>
+				<div id="timeline">
+					<div id="time" class="blue"></div>
+					<div id="time" class="grey"></div>
+					<div id="time" class="blue"></div>
+					<div id="time" class="grey"></div>
+					<div id="time" class="blue"></div>
+					<div id="time" class="grey"></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
